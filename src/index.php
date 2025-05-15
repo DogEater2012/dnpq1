d06aa9bbecc3fff605bbc2c59f67e74034ba2f9a
DogEater2012 <firenabang2012@icloud.com>
2025-05-15 11:33:17 +0900

    making main page

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>해병대 모병</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    
+
+    <p style = "margin-top:100px;"> <h1>해병대 모집</h1> </p>
+    <a href = "https://gall.dcinside.com/board/lists/?id=bitcoins_new1" target = "_blank" title = "marine gallary">Visit bitcoin gallary!</a>
+    <img src = "images/mumochill.jpg" width = "20%">
+    <p>hello<strong> my <u>ASSEY!</u> </strong> </p>
+    <input type = "button">
+    <p style = "margin-top:45px;"> Seki... Guiyeol!!!!!!!!!!!!!!!!!</p>
+</body>
+</html>
