e41b528b55ec2b40e2ae279554f5100f9ddb5b99
DogEater2012 <firenabang2012@icloud.com>
2025-06-04 21:40:26 +0900

    article_edit

---
+++ src/edit.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if(!isset($_POST['writter'])){
+        echo "<script>alert('작성자만 수정 가능하다 아쎄이!!!!!');</script>";
+       echo "<script> location.href = 'index.php'; </script>";
+       exit();
+    }
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>글쓰기</title>
+    </head>
+
+    <body>
+        <?php        
+        if(isset($_SESSION['username'])){
+            echo "반갑다!". $_SESSION['username']."아쎄이!!!!!!!<br>";
+        }
+        ?>
+
+        <form action = "process_make_article.php", method = "POST">
+            <p><label>title</label><input type = "text" name=  "title" placeholder = "제목"></p>
+            <p><label>description</label><textarea name=  "description" placeholder = "desc" ></textarea></p>
+            <p><input type = "submit" value = "제출"></p>
+        </form>
+    </body>
+
+</html>
