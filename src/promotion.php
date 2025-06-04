3b7bcb7b13703598a92ebdf717b9f96acf68b133
DogEater2012 <firenabang2012@icloud.com>
2025-06-04 16:26:56 +0900

    notice

--- src/promotion.php
+++ src/promotion.php
@@ -7,5 +7,19 @@
 <body>
     <img src = "images/대한민국해병대.png" width = "40%">
     <h1> 자랑스러운 대한민국 해병대를 소개합니다!</h1>
-    <h2> </h2>
+    <h2> 전설의 해병들!!!!</h2>
+
+    <ol>
+        <?php
+            $legend_marine = ['황근출','무모칠','톤톤정','함박아','박철곤','마철두','쾌흥태'];
+            $i = 0;
+            
+            while(isset($legend_marine[$i])){
+                echo "<li>".$legend_marine[$i]."</li>";
+                $i++;
+            }
+            
+        
+        ?>
+    </ol>
 </body>
