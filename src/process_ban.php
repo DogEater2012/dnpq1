70d6a73e88adab3e78568593f649a4b77ec6eecc
DogEater2012 <firenabang2012@icloud.com>
2025-06-05 16:27:54 +0900

    done_real_final_really_read_this_warning3_ban_done

--- src/process_ban.php
+++ src/process_ban.php
@@ -14,13 +14,15 @@
         <?php 
         $conn = mysqli_connect("assignment_template-db-1", "exampleuser", "examplepass", "exampledb");
         $article_id = $_POST['id'];
-        $sql2 = "UPDATE userinfo set warning = warning +1 WHERE username = '".$_POST['writter']."'";
-        $sql1 = "UPDATE article set is_clame = '1' WHERE id = '$article_id'";
-        $select = "SELECT * FROM userinfo WHERE username = '".$_POST['writter']."' ";
         
-        $result3 = mysqli_query($conn, $select);
+        $sql1 = "UPDATE article set is_clame = '1' WHERE id = '$article_id'";
+        mysqli_query($conn, $sql1);
+
+        $sql2 = "UPDATE userinfo set warning = warning +1 WHERE username = '".$_POST['writter']."'";
         $result2 = mysqli_query($conn, $sql2);
-        $result1 = mysqli_query($conn, $sql1);
+
+        $select = "SELECT * FROM userinfo WHERE username = '".$_POST['writter']."' ";
+        $result3 = mysqli_query($conn, $select);
         $user_ban = mysqli_fetch_assoc($result3);
         
         if($user_ban['warning'] >2){
